1e2186c8ea011c6eed22e1a05a32aa9c8da1f4af
aryayanroy <aryayanroy0@gmail.com>
2024-05-14 17:43:27 +0530

    init

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,19 @@
 $(document).ready(function(){
+    
+    var load_path = "assets/html/";
+
+    $("#page-header").load(load_path+"header.html",function(){
+        console.log("Header Loaded");
+    });
+
+    $("#bottom-menu").load(load_path+"aside.html",function(){
+        console.log("Bottom Aside Loaded");
+    });
+
+    $("#page-footer").load(load_path+"footer.html",function(){
+        console.log("Footer Loaded");
+    });
+
     $(window).scroll(function(){
         let scroll = $(this).scrollTop();
         if(scroll == 0){
@@ -7,5 +22,4 @@ $(document).ready(function(){
             $("header").css("box-shadow","0 6px 8px rgba(0, 0, 0, 0.4)");
         }
     })
-
 })
