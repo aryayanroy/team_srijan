056c9c39034c42f98d81b89b4914b1c347909da5
aryayanroy <aryayanroy0@gmail.com>
2024-05-14 18:05:08 +0530

    init

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,24 +2,34 @@ $(document).ready(function(){
     
     var load_path = "assets/html/";
 
-    $("#page-header").load(load_path+"header.html",function(){
-        console.log("Header Loaded");
+    $.get(load_path+"head.html", function(data){
+        $("head").prepend(data);
+        console.log("head loaded");
+    })
+
+    var header = $("#page-header");
+
+    header.load(load_path+"header.html",function(){
+        $(this).addClass("sticky-top");
+        console.log("header loaded");
     });
 
     $("#bottom-menu").load(load_path+"aside.html",function(){
-        console.log("Bottom Aside Loaded");
+        $(this).addClass("offcanvas offcanvas-bottom");
+        console.log("bottom aside loaded");
     });
 
     $("#page-footer").load(load_path+"footer.html",function(){
-        console.log("Footer Loaded");
+        $(this).addClass("px-3 py-5 px-sm-5 text-white");
+        console.log("footer loaded");
     });
 
     $(window).scroll(function(){
         let scroll = $(this).scrollTop();
         if(scroll == 0){
-            $("header").css("box-shadow","none");
+            header.css("box-shadow","none");
         }else{
-            $("header").css("box-shadow","0 6px 8px rgba(0, 0, 0, 0.4)");
+            header.css("box-shadow","0 6px 8px rgba(0, 0, 0, 0.4)");
         }
     })
 })
